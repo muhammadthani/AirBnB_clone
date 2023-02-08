6e7a186cdc0ff5168dea5b0f995e63572f8acd78
muhammadthani <thaniabdulkadir@gmail.com>
2023-02-08 06:58:05 -0800

    console.py

--- console.py
+++ console.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 """Command interpreter entry module"""
-
+from models.base_model import BaseModel
 import cmd
-class HBNB(cmd.Cmd):
+
+
+class HBNBCommand(cmd.Cmd):
     """This is the actual cmd code"""
     intro = "Command interpreter for AirBnB Project"
     prompt = "(hbnb) "
+
+    def do_create(self, line):
+        """Creates a new instance"""
+        if line == "" or line is None:
+            print(" ** class name missing ** ")
+        elif line not in storage.classes():
+            print(" ** class doesn't exist ** ")
+
+    def do_show(self, line):
+        """Prints the representation of an instance"""
+        if line == "" or line is None:
+            print(" ** class name missing ** ")
+        elif nline not in storage.classes():
+            print(" ** class doesn't exist **")
+
     def do_EOF(self, line):
         """Handles end of file Character"""
         print()
         return True
+
     def do_quit(self, line):
-        """Exits the program"""
+        """Quit command to exit the program"""
         return True
 
+
 if __name__ == '__main__':
-    HBNB().cmdloop()
+    HBNBCommand().cmdloop()
